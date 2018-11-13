a42f338fd2b6639d5ee4c798ed4e5e6f45b254da
tongqijie01 <1505478851@qq.com>
2018-11-13 15:06:52 +0800

    提交

--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,50 @@
-//gruntfile.js
-//模块化导入函数
+// gruntfile.js
+// 模块化导入函数
 module.exports = function(grunt){
-	//所有插件的配置信息
+	// 所有插件的配置信息
 	grunt.initConfig({
-		//获取package.json
+		// 获取package.json
 		pkg:grunt.file.readJSON('package.json'),
-		//gulify插件的配置信息
+		// gulify插件的配置信息
 		uglify:{
 			options:{
-				banner:'/*!<%= pkg.name %> <%= pkg.version %>发布日期:<%=grunt.template.today("yyyy-mm-dd")%>*/',
+				banner:'/*!<%=pkg.name%> <%=pkg.version%> 发布日期：<%=grunt.template.today("yyyy-mm-dd")%>*/',
 			},
-			build:{
-				src:"src/data.js",
-				dest:"dist/layout.min.js"
-			},
-			build2:{
+			dist:{
 				src:"src/js/wipe.js",
-				dest:"dist/js/wipe-<%= pkg.version %>.min.js"
+				dest:"dist/js/wipe-<%=pkg.version%>.min.js"
 			}
 		},
-		cssmin:{
-			options:{
-				mergeIntoShorthands:false,
-				roundingPrecision:-1
-			},
-			target:{
-				files:[{
-					expand:true,
-					cwb:"src/css",
-					src:['*.css'],
-					dest:'dist/css',
-					ext:'.min.css'
-				}]
-			}
-		},
+		// cssmin:{
+		// 	options:{
+		// 		mergeIntoShorthands:false,
+		// 		roundingPrecision:-1
+		// 	},
+		// 	target:{
+		// 		files:[{
+		// 			expand:true,
+		// 			cwd:'src/css',
+		// 			src:['*.css'],
+		// 			dest:'build/css',
+		// 			ext:'.min.css'
+		// 		}]
+		// 	}
+		// },
 		clean:{
 			dest:['dist/*']
 		},
 		jshint:{
 			test:['src/js/wipe.js'],
 			options:{
-				jshint:'.jshintrc'
+				jshintrc:'.jshintrc'
 			}
 		}
 	});
-	//告诉grunt需要使用插件
+	// 告诉grunt需要使用插件
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	//告诉grunt当我们输入grunt命令后需要做些什么，有先后顺序
-	grunt.registerTask('default',['jshint','clean','uglify']);
+	// 告诉grunt当我们输入grunt命令需要做些什么，有先后顺序
+	grunt.registerTask('default',['jshint','clean','uglify',]);
 }
